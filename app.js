36ee3d48fb948def9af19544c490d72d87a58576
Delpiero0424 <33252464+Delpiero0424@users.noreply.github.com>
2017-12-26 22:04:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var activityUtils    = require('./routes/activityUtils');
 var pkgjson = require( './package.json' );
 // Add fuel
 var fuel = require('fuel');
+var extend = require('extend');
 
 var app = express();
 
